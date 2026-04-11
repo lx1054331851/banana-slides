9e5acb0516d15e1bc2cd7e92cdb85e41f1d72d22
lee <lx1054331851@gmail.com>
2026-04-11 16:47:49 +0800

    修复Alembic多head并新增合并迁移

---
+++ backend/migrations/versions/5cfd92a42a41_merge_material_caption_head.py
@@ -0,0 +1,27 @@
+"""merge material caption head
+
+Revision ID: 5cfd92a42a41
+Revises: c153f8c4e111, d7b6f4e2c9ab
+Create Date: 2026-04-11 16:47:03.843195
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5cfd92a42a41'
+down_revision = ('c153f8c4e111', 'd7b6f4e2c9ab')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
+
+
+
